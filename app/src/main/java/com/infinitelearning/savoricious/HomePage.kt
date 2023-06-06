12c6550ffde4a188b80b652d34e8e4ecb7e6f997
bagas <12bagasrama@gmail.com>
2023-06-06 21:01:53 +0700

    tes1

--- app/src/main/java/com/infinitelearning/savoricious/HomePage.kt
+++ app/src/main/java/com/infinitelearning/savoricious/HomePage.kt
@@ -2,10 +2,45 @@ package com.infinitelearning.savoricious
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.infinitelearning.savoricious.databinding.ActivityHomePageBinding
 
 class HomePage : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomePageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.nb_home -> replaceFragment(Home())
+                R.id.nb_menu -> replaceFragment(Menu())
+                R.id.nb_add -> replaceFragment(Add())
+                R.id.nb_comunity -> replaceFragment(Comunity())
+                R.id.nb_profile -> replaceFragment(Profile())
+
+                else ->{
+
+
+                }
+
+            }
+            true
+
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fl_navbar,fragment)
+        fragmentTransaction.commit()
+
     }
 }
--- app/src/main/java/com/infinitelearning/savoricious/LandingPage3.kt
+++ app/src/main/java/com/infinitelearning/savoricious/LandingPage3.kt
@@ -1,11 +1,21 @@
 package com.infinitelearning.savoricious
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LandingPage3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing_page3)
+
+        val btn_daftar = findViewById<Button>(R.id.btn_daftar)
+
+        btn_daftar.setOnClickListener {
+            startActivity(Intent(this, HomePage::class.java))
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+
+        }
     }
 }
